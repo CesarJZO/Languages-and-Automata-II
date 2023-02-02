ebb1cec690092d59555813ec976b74b1cf020218
César <cesarjzo@outlook.com>
2023-02-02 11:32:05 -0600

    Add file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import math
 
+def quadratic_equation(a, b, c):
+    x1 = (-b + math.sqrt(b**2 - 4 * a *c)) / 2 * a
+    x2 = (-b - math.sqrt(b**2 - 4 * a *c)) / 2 * a
+    print("X1: ", x1, ", X2: ", x2)
+
 def cylinder_volume(height, radius):
     volume = height * math.pi * radius ** 2
     print(f"Volume: {volume}")
@@ -13,6 +18,10 @@ opt = (int)(input(menu))
 
 if opt == 1:
     quadratic_equation()
+    a = (float)(input("a: "))
+    b = (float)(input("b: "))
+    c = (float)(input("c: "))
+
 elif opt == 2:
     hypotenuse()
 elif opt == 3:
